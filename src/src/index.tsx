ff9ff6f6539c35ee4903f479dd3c65e65e42d571
Max Patiiuk <max@patii.uk>
2022-09-14 19:18:11 -0500

    Render a basic clickable button

--- src/src/index.tsx
+++ src/src/index.tsx
@@ -6,11 +6,20 @@ import ReactDOM from 'react-dom/client';
 import {App} from './components';
 import {Contexts} from './components/Core/Contexts';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-  <React.StrictMode>
-    <Contexts>
-      <App/>
-    </Contexts>
-  </React.StrictMode>
-);
+const container = document.querySelector('header div[aria-label="Search"]')?.parentElement?.parentElement ?? undefined;
+console.log({container});
+if(typeof container === 'object'){
+  const reactContainer = document.createElement('div');
+  container.prepend(reactContainer);
+  const root = ReactDOM.createRoot(reactContainer);
+  root.render(
+    <React.StrictMode>
+      <Contexts>
+        <App/>
+      </Contexts>
+    </React.StrictMode>
+  );
+}
+else
+  console.error('Failed to attach plugin');
+
