4a9513fbe46f9c0ca2acff3314a1f3e9585fb44d
Max Patiiuk <max@patii.uk>
2022-10-15 18:43:19 -0500

    Fix DoughnutChart rebuilding on any update

--- src/src/components/Charts/DoughnutChart.tsx
+++ src/src/components/Charts/DoughnutChart.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
 import { EventsStore } from '../EventsStore';
 import { CalendarsContext } from '../Contexts/CalendarsContext';
-import { RA, WritableArray } from '../../utils/types';
+import { RA, writable, WritableArray } from '../../utils/types';
 import {
   Chart,
   DoughnutController,
@@ -20,13 +20,37 @@ export function DoughnutChart({
 }): JSX.Element {
   const calendars = React.useContext(CalendarsContext);
   const labels = useLabels(calendars);
-  const dataSets = useDataSets(durations, calendars);
+
+  /**
+   * In order for ChartJS to smoothly animate from one state to another rather
+   * than re-render from scratch, need to mutate the data object rather than
+   * give it a new one
+   */
+  const { data, ...dataSet } = useDataSets(durations, calendars);
+  const dataRef = React.useRef(data);
+  const [chart, setChart] = React.useState<
+    Chart<'doughnut', RA<number>, string> | undefined
+  >(undefined);
+  React.useEffect(() => {
+    if (chart === undefined || dataRef.current === data) return;
+    Array.from(
+      {
+        length: Math.max(chart.data.datasets[0].data.length, data.length),
+      },
+      (_, index) => {
+        writable(chart.data.datasets[0].data)[index] = data[index];
+      }
+    );
+    chart.update();
+  }, [data, chart]);
+
   return (
     <Doughnut
       data={{
         labels,
-        datasets: [dataSets],
+        datasets: [{ ...dataSet, data: dataRef.current }],
       }}
+      ref={(chart) => setChart(chart ?? undefined)}
       options={{
         responsive: true,
         plugins: {
