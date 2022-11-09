cbf0dae3bb65c4aab48559f66f592d1cbd1d75b3
Durbatuluk1701 <52861844+Durbatuluk1701@users.noreply.github.com>
2022-11-09 13:55:04 -0600

    Update src/src/tests/__tests__/globalSetup.test.ts
    Co-authored-by: Jet Semrick  <40004095+jetsemr@users.noreply.github.com>

--- src/src/tests/__tests__/globalSetup.test.ts
+++ src/src/tests/__tests__/globalSetup.test.ts
@@ -2,6 +2,7 @@ test('make sure tests are running under correct time zone', () => {
   expect(Intl.DateTimeFormat().resolvedOptions().timeZone).toBe(
     'America/Chicago'
   );
+  /* temporary fix, will need to changed back to 300 to account for daylight savings time */
   expect(new Date().getTimezoneOffset()).toBe(360);
 });
 
