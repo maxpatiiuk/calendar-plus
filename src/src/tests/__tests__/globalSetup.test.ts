f3aae2309c1f54ab2483ae02bfe62187cc02bbd2
Max Patiiuk <max@patii.uk>
2022-11-14 20:35:26 -0600

    Fix test depending on daylights savings
    Fix #114
    
    Fixed by removing the test for time zone offset because this is actually
    a non issue.
    
    The test depends on daylights savings because it runs under users local
    time, rather than mocks the time
    
    Mocking the time would fix this issue.
    
    In places were consistent results are needed, the time is already
    mocked.
    
    Everywhere else, it is best not to mock time in order to better
    approximate real world conditions (e.g, would help detect the cases when
    some test fails on a given day of the week on in a given part of day)

--- src/src/tests/__tests__/globalSetup.test.ts
+++ src/src/tests/__tests__/globalSetup.test.ts
@@ -2,8 +2,6 @@ test('make sure tests are running under correct time zone', () => {
   expect(Intl.DateTimeFormat().resolvedOptions().timeZone).toBe(
     'America/Chicago'
   );
-  /* temporary fix, will need to changed back to 300 to account for daylight savings time */
-  expect(new Date().getTimezoneOffset()).toBe(360);
 });
 
 export {};
