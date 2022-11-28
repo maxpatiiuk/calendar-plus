41e2ffa224e235f4f505c2296f3a54fb35a87102
jet.semrick@gmail.com <jet.semrick@gmail.com>
2022-11-28 16:31:58 -0600

    fix time test for daylight savings

--- src/src/tests/__tests__/globalSetup.test.ts
+++ src/src/tests/__tests__/globalSetup.test.ts
@@ -2,7 +2,7 @@ test('make sure tests are running under correct time zone', () => {
   expect(Intl.DateTimeFormat().resolvedOptions().timeZone).toBe(
     'America/Chicago'
   );
-  expect(new Date().getTimezoneOffset()).toBe(300);
+  expect(new Date().getTimezoneOffset()).toBe(360);
 });
 
 export {};
